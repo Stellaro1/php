0927e0f4379c5e8cc6de337289815bbd4e311dc8
Wyine <148890732+Stellaro1@users.noreply.github.com>
2023-11-14 23:34:58 +0630

    home

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<style>
+body{
+    margin-top: 80px;
+}
+.href{
+    margin-left: 19px;
+}
+    a{
+        color: white;
+        text-decoration: none;
+
+    }
+</style>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-success">
+                   <div class="row">
+                    <div class="col-6"><a href=""> About Our Page</a></div>
+                    <div class="col-3"></div>
+                    <div class="col-3">
+                       <i> 
+                        <a href="reg.php" class="href">Register</a>
+                        <a href="login.php" class="href">Login</a></i>
+                    </div>
+                   </div>
+            </div>
+                <div class="card-body">
+                   
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum quasi impedit excepturi non sequi, cupiditate adipisci placeat similique itaque in pariatur asperiores ducimus. Reiciendis, animi quidem. Eos, quas ea!</p>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, atque minima temporibus odio doloribus quia sequi accusamus voluptas commodi dignissimos magnam architecto error aspernatur voluptates! Ducimus sint impedit perspiciatis. Pariatur!</p> 
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
